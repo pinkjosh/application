367825d943c727f97833ce6b44b983dd5bb768a9
pinkjosh <pinkjosh@gmail.com>
2020-07-14 17:49:14 -0500

    Update mydef.groovy

--- mydef.groovy
+++ mydef.groovy
@@ -1,8 +1,8 @@
 #!groovy
 
 def Deploy() {
-   sh '''#!/bin/bash -xe
-      echo "hello world!"
-      '''.stripIndent()
+   //sh '''#!/bin/bash -xe
+      sh '''echo "hello world!"'''
+     // '''.stripIndent()
 }
 return this;
