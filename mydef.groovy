99eadcb1cc7161dd3e198b4515f32dcca12e3ce7
pinkjosh <pinkjosh@gmail.com>
2020-07-14 17:57:44 -0500

    Update mydef.groovy

--- mydef.groovy
+++ mydef.groovy
@@ -1,6 +1,10 @@
 #!groovy
 
 def Deploy() {
-  'echo "hello world!"'.stripIndent().execute()
+  def proc = "echo 'hello world!!!'".execute()
+  def b = new StringBuffer()
+  proc.consumeProcessErrorStream(b)
+  println proc.text
+  println b.toString()
 }
 return this;
